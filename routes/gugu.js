14aae4fc78aa8a96a833259bd87df4bb6c100bd0
chenge <shawnnq@gmail.com>
2018-03-14 13:46:01 +0800

    add memobird sdk

--- routes/gugu.js
+++ routes/gugu.js
@@ -5,6 +5,8 @@ const moment = require('moment');
 const http = require('http');
 const querystring = require('querystring');
 const Iconv = require('iconv').Iconv;
+const Memobird = require('Memobird');
+const gm = require('gm');
 // const example_config = {
 //   ak: 'e272c99b552547718b06f745db8941d4',
 //   timestamp: moment().format('YYYY-MM-DD HH:mm:ss'),
@@ -13,6 +15,11 @@ const Iconv = require('iconv').Iconv;
 //   useridentifying: 'chenge',
 //   userID: 493476
 // };
+const memobird = new Memobird({
+  ak: 'e272c99b552547718b06f745db8941d4',
+  memobirdID: '5bcbce64967d923d',
+  useridentifying: 'chenge',
+});
 const config = {
   ak: 'e272c99b552547718b06f745db8941d4'
 };
@@ -35,7 +42,7 @@ router.get('/bind', (req, res, next) => {
   });
 });
 router.get('/print', (req, res, next) => {
-  let words = '中文测试test';
+  let words = '记得吃药';
   const iconv = new Iconv('UTF-8', 'gbk');
   words = iconv.convert(words);
   const content = Buffer.from(words).toString('base64');
@@ -57,5 +64,32 @@ router.get('/print', (req, res, next) => {
     });
   });
 });
+const encodeImage = function (data) {
+  return new Promise((resolve, reject) => {
+    console.info('~~~', data.length);
+    gm(data).resize(384).flip().type('Bilevel').colors(2).toBuffer('bmp', (error, buffer) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(`P:${buffer.toString('base64')}`);
+      }
+    });
+  });
+}
+
+function data2base64(data) {
+
+}
+router.get('/printpic', (req, res, next) => {
+  let url = 'http://oezn2ph4e.bkt.clouddn.com/IMG_4666.JPG';
+  request.get({ url, encoding: null }, (err, resp, body) => {
+    encodeImage(body).then((pic) => {
+      console.info('~~', pic);
+    });
+  });
+
+  // memobird.init()
+  // .then(() => memobird.printImage('http://7xrs2s.com1.z0.glb.clouddn.com/5388545BF2D3F99643AFE22BE8C87B8A.jpg'))
+})
 
 module.exports = router;
